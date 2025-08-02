cbd7940635dbfc904349a06aea91f6c8392e337b
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-08-02 13:22:26 -0300

    feat: add ServiceItem component to display barbershop services on BarbershopPage

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,6 +1,13 @@
+import ServiceItem from "@/app/_components/serviceItem"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
+import {
+  ChevronLeftIcon,
+  MapPinIcon,
+  MenuIcon,
+  SmartphoneIcon,
+  StarIcon,
+} from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { notFound } from "next/navigation"
@@ -15,6 +22,9 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
+    },
+    include: {
+      services: true,
     },
   })
 
@@ -50,7 +60,7 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           <MenuIcon />
         </Button>
       </div>
-
+      {/* Informações da Barbearia */}
       <div className="border-b border-solid p-5">
         <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
 
@@ -59,16 +69,41 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           <p className="text-sm">{barbershop.address}</p>
         </div>
 
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-2">
           <StarIcon className="size-18 fill-primary text-primary" />
           <p className="text-sm">5,0 (889 avaliações)</p>
         </div>
       </div>
 
       {/* Descrição */}
+      <div className="space-y-2 border-b border-solid p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
+        <p className="text-justify text-sm">{barbershop.description}</p>
+      </div>
+
+      {/* Serviços */}
       <div className="space-y-3 border-b border-solid p-5">
-        <h2 className="font-bold uppercase text-gray-400">Sobre nós</h2>
-        <p className="text-sm">{barbershop.description}</p>
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
+      </div>
+
+      <div className="spcace-y-3 p-5">
+        {barbershop.phones.map((phone) => (
+          <div className="flex justify-between" key={phone}>
+            <div className="flex items-center gap-2">
+              <SmartphoneIcon />
+              <p className="text-sm">{phone}</p>
+            </div>
+
+            <Button variant="outline" size="sm">
+              Copiar
+            </Button>
+          </div>
+        ))}
       </div>
     </div>
   )
